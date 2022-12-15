a05daa088ebacc635bc7b8eea0b0f40f14ab4d0a
HooniKim <tpgns2289@naver.com>
2022-12-15 22:11:32 +0900

    a

--- myagora/src/Form.js
+++ myagora/src/Form.js
@@ -6,25 +6,25 @@
 export const Form = () => {
     return (
 <section className="form__container">
-      <form action="" method="get" className="form">
+    <form action="" method="get" className="form">
         <div className="form__input--wrapper">
-          <div className="form__input--name">
+        <div className="form__input--name">
             <label htmlFor="name">Enter your name: </label>
             <input type="text" name="name" id="name" required=""></input>
-          </div>
-          <div className="form__input--title">
+        </div>
+        <div className="form__input--title">
             <label htmlFor="name">Enter your title: </label>
             <input type="text" name="name" id="name" required=""></input>
-          </div>
-          <div className="form__textbox">
+        </div>
+        <div className="form__textbox">
             <label htmlFor="story">Your question: </label>
             <textarea id="story" name="story" placeholder="질문을 작성하세요" required=""></textarea>
-          </div>
+        </div>
         </div>
         <div className="form__submit">
-          <button className="submit_button">submit</button>
+        <button className="submit_button">submit</button>
         </div>
-      </form>
+    </form>
     </section>
-    )
+    ) 
 }
